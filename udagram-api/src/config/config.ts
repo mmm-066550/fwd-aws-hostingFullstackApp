950257ab38d98a9b650fb4e68f451b7e614e63d5
mmm-066550 <95170213+mmm-066550@users.noreply.github.com>
2022-06-07 17:52:01 +0200

    update backend files

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -7,16 +7,16 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  db_port: process.env.POSTGRES_DB_PORT,
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   port: Number(process.env.PORT),
+  host: process.env.POSTGRES_HOST,
   dialect: process.env.AWS_RDS_DIALECT,
-  aws_region: process.env.AWS_DEFAULT_REGION,
+  aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -2,48 +2,58 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import { sequelize } from "./sequelize";
+
 import { IndexRouter } from "./controllers/v0/index.router";
+
 import bodyParser from "body-parser";
 import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
-dotenv.config();
-const app = express();
-app.use(bodyParser.json());
-app.use(
-  cors({
-    allowedHeaders: [
-      "Origin",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "X-Access-Token",
-      "Authorization",
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Methods",
-    ],
-    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    preflightContinue: true,
-    origin: "*",
-  })
-);
+(async () => {
+  dotenv.config();
 
-// Root URI call
-app.get("/", async (req, res) => {
-  res.send("/api/v0/");
-});
-app.use("/api/v0/", IndexRouter);
+  await sequelize.addModels(V0_FEED_MODELS);
+  await sequelize.addModels(V0_USER_MODELS);
+  await sequelize.sync();
 
-// (async () => {
-// await sequelize.addModels(V0_FEED_MODELS);
-// await sequelize.addModels(V0_USER_MODELS);
-// await sequelize.sync();
-// console.log("Database Connected");
-// })();
+  console.log("Database Connected");
 
-// Start the Server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Backend server is listening on port ${port}....`);
-  console.log(`press CTRL+C to stop server`);
-});
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(bodyParser.json());
+
+  // app.use(cors());
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS.
+  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Methods",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      origin: "*",
+    })
+  );
+
+  app.use("/api/v0/", IndexRouter);
+
+  // Root URI call
+  app.get("/", async (req, res) => {
+    res.send("/api/v0/");
+  });
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`Backend server is listening on port ${port}....`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
